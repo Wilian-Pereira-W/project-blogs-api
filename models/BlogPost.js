30b57c8c8ac1e107e2344e22432cbf725170666e
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-14 10:49:56 -0300

    Implementando o models do BlogPost

---
+++ models/BlogPost.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  published: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  updated: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+};
+
+module.exports = (sequelize) => {
+  const BlogPost = sequelize.define('BlogPost',
+    attributes,
+    {
+      underscored: true,
+      timestamps: false,
+      tableName: 'BlogPosts',
+    });
+
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.BlogPost, { foreignKey: 'userId', as: 'user' });
+    };
+
+    return BlogPost;
+};
