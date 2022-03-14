5e3f287087c570785bcb4f3a18dc505bc1507d1d
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-14 17:00:00 -0300

    Implementando a validação do email do login

---
+++ middlewares/validateEmailLogin.js
@@ -0,0 +1,16 @@
+const validateEmailLogin = (req, res, next) => {
+  const { email } = req.body;
+  if (email === undefined) {
+    return res.status(400).json({
+        message: '"email" is required',
+      }); 
+  }
+  if (email.length === 0) {
+    return res.status(400).json({
+        message: '"email" is not allowed to be empty',
+    }); 
+  }
+  next();
+};
+
+module.exports = validateEmailLogin;
