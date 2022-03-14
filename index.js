8894ced10d758f64f4f9548802c9a8235f66c802
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-14 15:03:26 -0300

    Adicionando Requisito 1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const { create } = require('./controllers/userController');
+const {
+  validateDisplayName,
+  validateEmail,
+  validatePassword,
+} = require('./middlewares');
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/user', validateDisplayName, validateEmail, validatePassword, create);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
