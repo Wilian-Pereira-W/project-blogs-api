cf5869b3a289feff140d8a9064ec0cd1ae71e795
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-14 17:00:48 -0300

    Adicionando o Requisito 2

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,13 @@
 const validateDisplayName = require('./validateDisplayName');
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
+const validatePasswordLogin = require('./validatePasswordLogin');
+const validateEmailLogin = require('./validateEmailLogin');
 
 module.exports = {
   validateDisplayName,
   validateEmail,
   validatePassword,
+  validatePasswordLogin,
+  validateEmailLogin,
 };
