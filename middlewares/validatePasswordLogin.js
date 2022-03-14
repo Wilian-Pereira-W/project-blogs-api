65f6402c1b63038bca03de1cefeef12322fb9389
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-14 17:00:16 -0300

    Implementando a validação do password do login

---
+++ middlewares/validatePasswordLogin.js
@@ -0,0 +1,18 @@
+const validatePasswordLogin = (req, res, next) => {
+  const { password } = req.body;
+
+  if (password === undefined) {
+    return res.status(400).json({
+      message: '"password" is required',
+    }); 
+  }
+
+  if (password.length === 0) {
+    return res.status(400).json({
+        message: '"password" is not allowed to be empty',
+      }); 
+  }
+next();
+};
+
+module.exports = validatePasswordLogin;
