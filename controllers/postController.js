6c97d7b5de03ee2aaf513a9d2af91e1a54c861b3
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-16 09:55:54 -0300

    Implementando o findOne do post

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,6 +10,21 @@ const findAll = async (req, res) => {
   return res.status(200).json(blogPostList);
 };
 
+const findOne = async (req, res) => {
+  const { id } = req.params;
+  console.log('id::', id);
+  const post = await BlogPost.findByPk(id, {
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Categorie, as: 'categories', through: { attributes: [] } },
+     ],
+  });
+  if (!post) return res.status(404).json({ message: 'Post does not exist' });
+  console.log(post);
+  return res.status(200).json(post);
+};
+
 module.exports = {
   findAll,
+  findOne,
 };
